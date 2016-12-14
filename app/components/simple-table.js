517abfad4e901b49c083debb663486cdb11c82d7
Sunil Soni <sunilsoni@live.in>
2016-12-14 16:40:58 +0530

    Ember js 2 tutorial with Ember Light Table

---
+++ app/components/simple-table.js
@@ -0,0 +1,46 @@
+// BEGIN-SNIPPET selectable-table
+import Ember from 'ember';
+import TableCommon from '../mixins/table-common';
+
+const {
+  computed
+} = Ember;
+
+export default Ember.Component.extend(TableCommon, {
+  hasSelection: computed.notEmpty('table.selectedRows'),
+
+  columns: computed(function() {
+    return [{
+      label: 'First Name',
+      valuePath: 'firstName',
+      width: '150px'
+    }, {
+      label: 'Last Name',
+      valuePath: 'lastName',
+      width: '150px'
+    }, {
+      label: 'Address',
+      valuePath: 'address'
+    }, {
+      label: 'State',
+      valuePath: 'state'
+    }, {
+      label: 'Country',
+      valuePath: 'country'
+    }];
+  }),
+
+  actions: {
+    selectAll() {
+      this.get('table.rows').setEach('selected', true);
+    },
+
+    deselectAll() {
+      this.get('table.selectedRows').setEach('selected', false);
+    },
+
+    deleteAll() {
+      this.get('table').removeRows(this.get('table.selectedRows'));
+    }
+  }
+});
