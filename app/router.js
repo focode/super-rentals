bc6a5fca6af69d0edd8ba2a2f21f4976b2b16083
Sunil Soni <sunilsoni@live.in>
2016-12-14 16:46:33 +0530

    Revert "Ember js 2 tutorial with Ember Light Table"
    This reverts commit 517abfad4e901b49c083debb663486cdb11c82d7.

--- app/router.js
+++ app/router.js
@@ -2,14 +2,11 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
-  this.route('about');
-  this.route('contact');
-  this.route('rentals');
-  this.route('tables');
 });
 
 export default Router;
